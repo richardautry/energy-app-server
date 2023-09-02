2f0a7c75e063bf5bc1756c177ceecb65e84f0232
Richard Autry <autry.richard@gmail.com>
2023-09-02 10:14:27 -0400

    Current peak demand hours now printing via function

--- src/energy_demand.rs
+++ src/energy_demand.rs
@@ -6,30 +6,6 @@ use chrono::Local;
 use chrono::Duration;
 use reqwest;
 
-// Use the following python function to calculate peak hours
-// def find_peak_hour_timeframe(demand_data):
-//     """
-//     Given demand data from the EIA API
-//     Find the consecutive hour blocks that constitute roughly 20% of daily energy demand
-//     i.e. 2pm - 6pm for the East Coast
-//     """
-//     highest_energy_hour = max(demand_data, key=lambda data: data.value)
-//     current_index = demand_data.index(highest_energy_hour)
-//     left_index = current_index - 1
-//     right_index = current_index + 1
-//     peak_hours_percentage = highest_energy_hour.value / total_megawatt_hours
-    
-//     while peak_hours_percentage < 0.20 and left_index > -1 and right_index < len(demand_data):
-//         left_value = demand_data[left_index].value
-//         right_value = demand_data[right_index].value
-//         if left_value > right_value:
-//             peak_hours_percentage += (left_value / total_megawatt_hours)
-//             left_index -= 1
-//         else:
-//             peak_hours_percentage += (right_value / total_megawatt_hours)
-//             right_index += 1
-//     return demand_data[left_index], demand_data[right_index], peak_hours_percentage
-
 pub async fn find_peak_hour_timeframe() -> Result<Vec<EIAData>, reqwest::Error>{
     let mut result: Vec<EIAData> = Vec::new();
     let json_result = get_eia_data().await?;
@@ -41,13 +17,6 @@ pub async fn find_peak_hour_timeframe() -> Result<Vec<EIAData>, reqwest::Error>{
     let local_datetime = Local::now() - Duration::days(1);
     let current_day_str = local_datetime.format("%Y-%m-%d").to_string();
 
-    // let current_day_index_start = data.iter().position(
-    //     |d| d.period.contains(&current_day_str) && d.r#type == "DF"
-    // ).unwrap();
-    // let current_day_index_end = &data[current_day_index_start..].iter().position(
-    //     |d| !d.period.contains(&current_day_str)
-    // ).unwrap();
-
     let current_day_data: Vec<&EIAData> = data.iter().filter(
         |d| d.period.contains(&current_day_str) && d.r#type == "DF"
     ).collect();
@@ -55,23 +24,34 @@ pub async fn find_peak_hour_timeframe() -> Result<Vec<EIAData>, reqwest::Error>{
     let high_hour_data = current_day_data.iter().max_by_key(|d| d.value).unwrap();
     let high_hour_index = current_day_data.iter().position(|d| d.value == high_hour_data.value).unwrap();
     let total_megawatt_hours = current_day_data.iter().map(|d| d.value).reduce(|acc, v| acc + v).unwrap();
-    let mut peak_hours_percentage = (high_hour_data.value / total_megawatt_hours) as f32;
-    
+    let mut peak_hours_percentage = high_hour_data.value as f32 / total_megawatt_hours as f32;
+
     let mut left_vec = Vec::from(&current_day_data[0..high_hour_index]);
     left_vec.reverse();
-    let left_iter = left_vec.iter();
-    let right_iter = &current_day_data[high_hour_index + 1..].iter();
-    // let mut left_index = high_hour_index - 1;
-    // let mut right_index = high_hour_index + 1;
+    let mut left_iter = left_vec.iter().peekable();
+    let right_vec = Vec::from(&current_day_data[high_hour_index + 1..]);
+    let mut right_iter = right_vec.iter().peekable();
+    let mut left_data = left_iter.next().unwrap();
+    let mut right_data = right_iter.next().unwrap();
+    let mut left_index = high_hour_index - 1;
+    let mut right_index = high_hour_index + 1;
 
-    while peak_hours_percentage > 0.2 {
-        // TODO: There's probably a smarter way to do this using two iterators
-        
+    while peak_hours_percentage < 0.2 && left_iter.peek().is_some() && right_iter.peek().is_some() {
+        // TODO: Should probably redo this loop logic to check for all states
+        if left_data.value > right_data.value {
+            peak_hours_percentage += left_data.value as f32 / total_megawatt_hours as f32;
+            left_data = left_iter.next().unwrap();
+            left_index -= 1;
+        } else {
+            peak_hours_percentage += right_data.value as f32 / total_megawatt_hours as f32;
+            right_data = right_iter.next().unwrap();
+            right_index += 1;
+        }
     }
 
-    // TODO: Implement loop on data here to find peak hours
-
     println!("{:?}", high_hour_data);
+    println!("peak_hours_percentage: {:.2}", peak_hours_percentage);
+    println!("Left Index {}, Right Index {}", left_index, right_index);
 
     Ok(data)
 }
