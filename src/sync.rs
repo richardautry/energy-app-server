6d832478c72e6531a123b78e0b815dc284689483
Richard Autry <autry.richard@gmail.com>
2023-09-03 12:14:50 -0400

    Starting sync module for syncing devices to grid schedule

---
+++ src/sync.rs
@@ -0,0 +1,6 @@
+// TODO:
+// Create async function that spawns a task which will
+// check the for the current time every hour (or 5 minutes maybe)
+// If the time is inside the high demand window, turn off the device
+// If the time is outside the high demand window, turn on the device
+// Check state before issuing command to avoid unnecessary commands
