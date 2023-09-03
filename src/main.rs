75e2d1f116d9b95e9a898666ff49a5ad37d589c1
Richard Autry <autry.richard@gmail.com>
2023-09-03 18:05:09 -0400

    Working version of server as device controller complete

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ mod timer;
 mod device;
 mod eia_client;
 mod energy_demand;
+mod sync;
 
 use axum::{
     routing::{get, post},
@@ -13,8 +14,8 @@ use device::{
     turn_on_device,
     turn_off_device,
 };
-use eia_client::get_eia_data;
 use energy_demand::find_peak_hour_timeframe;
+use sync::start_sync_with_energy_demand;
 
 #[tokio::main]
 async fn main() {
@@ -28,7 +29,8 @@ async fn main() {
         .route("/devices", get(device_data))
         .route("/devices/turn_on", post(turn_on_device))
         .route("/devices/turn_off", post(turn_off_device))
-        .route("/devices/set_timer", post(start_timer_device));
+        .route("/devices/set_timer", post(start_timer_device))
+        .route("/devices/sync_with_demand", post(start_sync_with_energy_demand));
         //.route("/sleep/:id", get(move |path| sleep_and_print(path, &tx)));
 
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
--- src/sync.rs
+++ src/sync.rs
@@ -1,6 +1,55 @@
-// TODO:
-// Create async function that spawns a task which will
-// check the for the current time every hour (or 5 minutes maybe)
-// If the time is inside the high demand window, turn off the device
-// If the time is outside the high demand window, turn on the device
-// Check state before issuing command to avoid unnecessary commands
+use chrono::Local;
+use crate::energy_demand::find_peak_hour_timeframe;
+use crate::device::{
+    turn_off_device,
+    turn_on_device,
+    SimpleDeviceData
+};
+use axum::{
+    http::StatusCode,
+    Json,
+};
+use serde::de;
+use tokio::time;
+
+pub async fn start_sync_with_energy_demand(
+    Json(payload): Json<SimpleDeviceData>,
+) -> (StatusCode, Json<bool>) {
+    // TODO:
+    // Create async function that spawns a task which will
+    // check the for the current time every hour (or 5 minutes maybe)
+    // If the time is inside the high demand window, turn off the device
+    // If the time is outside the high demand window, turn on the device
+    // Check state before issuing command to avoid unnecessary commands
+    tokio::spawn(async move {
+        let mut interval = time::interval(time::Duration::from_secs(5 * 60));
+        let demand_times = find_peak_hour_timeframe().await.unwrap();
+        let start_time = demand_times.0;
+        let end_time = demand_times.1;
+        let alias = &payload.alias;
+        let mac = &payload.mac;
+        loop {
+            interval.tick().await;
+            let local_time = Local::now();
+            println!("tick: Five minutes elapsed {}", local_time);
+
+            if local_time > start_time && local_time < end_time {
+                println!("Time inside high demand window. Turning device {} off", alias);
+                let simple_device_data = SimpleDeviceData {
+                    alias: alias.to_owned(),
+                    mac: mac.to_owned()
+                };
+                turn_off_device(Json(simple_device_data)).await;
+            } else {
+                println!("Time outside high demand window. Turning device {} on", alias);
+                let simple_device_data = SimpleDeviceData {
+                    alias: alias.to_owned(),
+                    mac: mac.to_owned()
+                };
+                turn_on_device(Json(simple_device_data)).await;
+            }
+        }
+    });
+
+    (StatusCode::OK, Json(true))
+}
